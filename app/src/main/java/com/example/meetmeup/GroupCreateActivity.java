d7bf2f6e365e16c6d3c3520dd0ad58bacbe2705a
boldalex <46719335+boldalex@users.noreply.github.com>
2020-11-12 13:18:37 -0500

    alex + olex

--- app/src/main/java/com/example/meetmeup/GroupCreateActivity.java
+++ app/src/main/java/com/example/meetmeup/GroupCreateActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GroupCreateActivity extends AppCompatActivity {
+
+    private LinearLayout members;
+    private EditText txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_create);
+        members = findViewById(R.id.added);
+        txtEmail = findViewById(R.id.txtEmail);
     }
 
     public void create(View view) {
+    }
+
+    public void addMember(View view) {
+        TextView newMember = new TextView(this);
+        newMember.setText(txtEmail.getText());
+        members.addView(newMember);
+        txtEmail.setText("");
     }
 }
---
+++ app/src/main/java/com/example/meetmeup/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.meetmeup;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.meetmeup.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    Button bLoggin, bSignup, bSignupNewAccount, bLoginLogin;
+    FirebaseDatabase db; //For Connection to DataBase
+    FirebaseAuth auth; // For Authorization
+    DatabaseReference users; // For work with tables in DB
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initialisation
+        bLoggin = findViewById(R.id.bLoggin);
+        bSignup = findViewById(R.id.bSignup);
+        bSignupNewAccount = findViewById(R.id.bSignupNewAccount);
+        bLoginLogin = findViewById(R.id.bLoginLogin);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users"); //"Users" is a name of table
+
+        bLoggin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoginWindow();
+            }
+        });
+        bSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCreateNewAccountWindow();
+            }
+        });
+    }
+    private void showLoginWindow(){
+
+    }
+    private void showCreateNewAccountWindow(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Create new account");
+        dialog.setMessage("Enter your data");
+
+        //take "create_new_account" and put it in "dialog"
+        LayoutInflater inflater = LayoutInflater.from(this); //create object inflater
+        View createNewAccount_Window = inflater.inflate(R.layout.activity_signup, null); //put window in inflater
+        dialog.setView(createNewAccount_Window);
+
+        //take the data from reg fields
+        // MaterialTextInputPicker email = createNewAccount_Window.findViewById(R.id.editTextTextEmailAddress2);
+        EditText text = (EditText)findViewById(R.id.editTextTextEmailAddress2);
+        String value = text.toString();
+
+    }
+}
